909ec2aa1f382f7629c534c94f18b5eb5f95ce08
Marco Bravo <marco.bravo@sysadmin1.int.planbdigital.com>
2017-08-24 14:34:28 -0400

    Playing with hello example

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Printf("Hello, world! *\n")
+
+	current_time := time.Now().Local()
+	fmt.Println("The Current time is ", current_time.Format("2006-01-02"))
+	
+	current_time := time.Now().UTC()
+    fmt.Println("The Current time is ", current_time.Format("2006-01-02 MST"))
 }
