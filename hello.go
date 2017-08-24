511b3648cc64cf0337f48ccc2a7c33801f43f541
Marco Bravo <marco.bravo@sysadmin1.int.planbdigital.com>
2017-08-24 14:43:18 -0400

    More adjustments

--- hello.go
+++ hello.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello, world! *\n")
+	fmt.Printf("Hello, world! \\o/\n")
 
 	currentLocalTime := time.Now().Local()
-	fmt.Println("The Current time is ", currentLocalTime.Format("2006-01-02"))
+	fmt.Println("The Current time is ", currentLocalTime.Format("2006-01-02 MST 15:04:05"))
 
 	currentUTCtime := time.Now().UTC()
-	fmt.Println("The Current time is ", currentUTCtime.Format("2006-01-02 MST"))
+	fmt.Println("The Current time is ", currentUTCtime.Format("2006-01-02 MST 15:04:05"))
 }
